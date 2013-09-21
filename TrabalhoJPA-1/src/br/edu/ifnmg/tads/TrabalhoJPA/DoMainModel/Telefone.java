fea79a9cb313b215817f7057e20cb2d890c4dcf7
Maike Jordan <>
2013-09-21 18:54:49 -0300

    Criação e mapeamento de Classes

--- TrabalhoJPA-1/src/br/edu/ifnmg/tads/TrabalhoJPA/DoMainModel/Telefone.java
+++ TrabalhoJPA-1/src/br/edu/ifnmg/tads/TrabalhoJPA/DoMainModel/Telefone.java
@@ -5,6 +5,7 @@
 package br.edu.ifnmg.tads.TrabalhoJPA.DoMainModel;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,20 +20,37 @@ public class Telefone implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long idTelefone;
+    
+    @Column(name="numero")
+    private int numero;
 
-    public Long getId() {
-        return id;
+    public Telefone(Long idTelefone, int numero) {
+        this.idTelefone = idTelefone;
+        this.numero = numero;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    
+    public Long getIdTelefone() {
+        return idTelefone;
     }
 
+    public void setIdTelefone(Long idTelefone) {
+        this.idTelefone = idTelefone;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+   
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (idTelefone != null ? idTelefone.hashCode() : 0);
         return hash;
     }
 
@@ -43,7 +61,7 @@ public class Telefone implements Serializable {
             return false;
         }
         Telefone other = (Telefone) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.idTelefone == null && other.idTelefone != null) || (this.idTelefone != null && !this.idTelefone.equals(other.idTelefone))) {
             return false;
         }
         return true;
@@ -51,7 +69,7 @@ public class Telefone implements Serializable {
 
     @Override
     public String toString() {
-        return "br.edu.ifnmg.tads.TrabalhoJPA.DoMainModel.Telefone[ id=" + id + " ]";
+        return "br.edu.ifnmg.tads.TrabalhoJPA.DoMainModel.Telefone[ id=" + idTelefone + " ]";
     }
     
 }
